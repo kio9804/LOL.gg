35279d544ea9ca8d3e1512eaf451c73cb30a2195
Seonho Kim <dev.sensational@gmail.com>
2021-11-20 15:59:46 +0900

    매치 히스토리 추가

--- app/src/main/java/com/example/helloandroid/ThreadTest.java
+++ app/src/main/java/com/example/helloandroid/ThreadTest.java
@@ -4,8 +4,9 @@ public class ThreadTest extends Thread{
     public void run(){
             try {
                 Thread.sleep(2000);
-                System.out.println("Test Class : " + DataHandelObject.summonerIds.getId());
-                System.out.println("Test Class : " + DataHandelObject.leagueInfos.get(0).getTier());
+                System.out.println("Test Class : " + DataHandlerObject.summonerIds.getId());
+                System.out.println("Test Class : " + DataHandlerObject.leagueInfos.get(0).getTier());
+                System.out.println("Test Class : " + DataHandlerObject.leagueInfos.get(0).getRank());
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
